3de26dcea5e87b5cbe1dcb3e551be2c4335e31cf
Tejan Singh <tstejan@gmail.com>
2022-10-04 05:44:37 +0530

    feat: added hypotenuse calculation functionaliyt

---
+++ hypotenuse.js
@@ -0,0 +1,31 @@
+const baseInput = document.querySelector(".base-input-field");
+const heightInput = document.querySelector(".height-input-field");
+const warningMsg = document.querySelector("#warning");
+const resultMsg = document.querySelector("#result");
+const calculateBtn = document.querySelector(".btn");
+
+const calculateHypotenuse = (baseValue, heightValue) => {
+  const hypotenuse = Math.sqrt(
+    baseValue * baseValue + heightValue * heightValue
+  );
+  return hypotenuse;
+};
+
+const handleClick = () => {
+  const baseValue = Number(baseInput.value);
+  const heightValue = Number(heightInput.value);
+
+  if (baseValue > 0 && heightValue > 0) {
+    warningMsg.innerHTML = "";
+
+    const result = calculateHypotenuse(baseValue, heightValue);
+
+    resultMsg.innerHTML = "The length of hypotenuse is " + result;
+    return;
+  } else {
+    warningMsg.innerHTML = "Please enter correct values";
+    resultMsg.innerHTML = "";
+  }
+};
+
+calculateBtn.addEventListener("click", handleClick);
